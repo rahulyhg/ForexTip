36ff4adc28c6c4e034f834b8b1319ac538ef6454
TramPhan <tram.phan@mor.com.vn>
2017-12-07 11:18:12 +0700

    Update

--- rest.inc.php
+++ rest.inc.php
@@ -6,61 +6,67 @@
  * (c) 2014 Jordi Moraleda
  */
 
-Class RestCurl {
-    public static function exec($method, $url, $obj = array()) {
+Class RestCurl
+{
+  public static function exec($method, $url, $obj = array())
+  {
 
-        $curl = curl_init();
+    $curl = curl_init();
 
-        switch($method) {
-            case 'GET':
-                if(strrpos($url, "?") === FALSE) {
-                    $url .= '?' . http_build_query($obj);
-                }
-                break;
-
-            case 'POST':
-                curl_setopt($curl, CURLOPT_POST, TRUE);
-                curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($obj));
-                break;
-
-            case 'PUT':
-            case 'DELETE':
-            default:
-                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, strtoupper($method)); // method
-                curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($obj)); // body
+    switch ($method) {
+      case 'GET':
+        if (strrpos($url, "?") === FALSE) {
+          $url .= '?' . http_build_query($obj);
         }
+        break;
 
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HEADER, TRUE);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, TRUE);
+      case 'POST':
+        curl_setopt($curl, CURLOPT_POST, TRUE);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($obj));
+        break;
 
-        // Exec
-        $response = curl_exec($curl);
-        $info = curl_getinfo($curl);
-        curl_close($curl);
-
-        // Data
-        $header = trim(substr($response, 0, $info['header_size']));
-        $body = substr($response, $info['header_size']);
-
-        return array('status' => $info['http_code'], 'header' => $header, 'data' => json_decode($body));
+      case 'PUT':
+      case 'DELETE':
+      default:
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, strtoupper($method)); // method
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($obj)); // body
     }
 
-    public static function get($url, $obj = array()) {
-        return RestCurl::exec("GET", $url, $obj);
-    }
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HEADER, TRUE);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, TRUE);
 
-    public static function post($url, $obj = array()) {
-        return RestCurl::exec("POST", $url, $obj);
-    }
+    // Exec
+    $response = curl_exec($curl);
+    $info = curl_getinfo($curl);
+    curl_close($curl);
 
-    public static function put($url, $obj = array()) {
-        return RestCurl::exec("PUT", $url, $obj);
-    }
+    // Data
+    $header = trim(substr($response, 0, $info['header_size']));
+    $body = substr($response, $info['header_size']);
 
-    public static function delete($url, $obj = array()) {
-        return RestCurl::exec("DELETE", $url, $obj);
-    }
+    return array('status' => $info['http_code'], 'header' => $header, 'data' => json_decode($body));
+  }
+
+  public static function get($url, $obj = array())
+  {
+    return RestCurl::exec("GET", $url, $obj);
+  }
+
+  public static function post($url, $obj = array())
+  {
+    return RestCurl::exec("POST", $url, $obj);
+  }
+
+  public static function put($url, $obj = array())
+  {
+    return RestCurl::exec("PUT", $url, $obj);
+  }
+
+  public static function delete($url, $obj = array())
+  {
+    return RestCurl::exec("DELETE", $url, $obj);
+  }
 }
